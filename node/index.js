a675de169cef065fbaf598efd214c082ee8fa57a
jungmin <43zhuzhu@gmail.com>
2022-06-04 14:34:15 +0900

    프론트에서 백단요청

--- node/index.js
+++ node/index.js
@@ -7,13 +7,15 @@ const port = 5000;
 // express에서 react폴더 안쪽의 build폴더를 static으로 지정
 app.use(express.static(path.join(__dirname, '../react/build')));
 
-// 클라이언트에서 보내는 데이터를 받도록 설정(body-parser)
 app.use(express.json());
-app.use(express.urlencoded({extneded: true}));
+app.use(express.urlencoded({extended: true}));
+
+// Post모델 불러옴
+const {post} = require('./model/postSchema.js');
 
 app.listen(port, () => {
 	mongoose
-	.connect('mongodb+srv://Jujungmin:zxcvzmfhd2!@cluster0.n0zcs.mongodb.net/?retryWrites=true&w=majority')
+	.connect('mongodb+srv://Jujungmin:zxcvzmfhd2!@cluster0.n0zcs.mongodb.net/reactNode?retryWrites=true&w=majority')
 	.then(() => {
 	console.log('Server Listening on port:' + port);
 	console.log('mongoDB success')
@@ -28,7 +30,19 @@ app.get('/', (req, res) => {
 });
 
 // react로부터 받은 요청처리
-app.post('/api/send', (req, res) => {
+app.post('/api/create', (req, res) => {
 	console.log(req.body);
-	res.status(200).json({success: true, result: req.body.name + '2'});
+	
+	const PostModel = new Post({
+		title: req.body.title,
+		content: req.body.content
+	});
+
+	PostModel.save.then(() => {
+		res.status(200).json({success: true});
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(400).json({success: false});
+	})
 });
--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,46 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-	const item = {
-		name: 'David'
-	}
-	useEffect(() => {
+	const [tit, setTit] = useState('');
+	const [con, setCon] = useState('');
+
+	const handleCreate = () => {
+		const item = {
+			title: tit,
+			content: con,
+		};
+
 		axios
-		.post('/api/send', item)
+		.post('/api/create', item)
 		.then((res) => {
 			console.log(res);
-		}).catch((err) => {
+			if(res.data.success) {
+				alert('데이터 저장에 성공했습니다.');
+			} else {
+				alert('데이터 저장에 실패했습니다.');
+			}
+		})
+		.catch((err) => {
 			console.log(err);
 		})
-	}, []);
+	};
 
-	return <h1>React Project</h1>;
+	useEffect(() => {}, []);
+
+	return (
+		<section>
+			<label htmlFor='tit'>Title</label>
+			<br />
+			<input type='text' id='tit' value={tit} onChange={(e) => setTit(e.target.value)} />
+			<br />
+			<label htmlFor='con'>Content</label>
+			<br />
+			<textarea id='con' value={con} onChange={(e) => setCon(e.target.value)}></textarea>
+			<br />
+			<button onClick={handleCreate}>SEND</button>
+		</section>
+	)
 }
 
 export default App;
