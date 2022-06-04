4fa337ec6960fed09d325761e9070ebd9f92ccfc
jungmin <43zhuzhu@gmail.com>
2022-06-04 15:27:54 +0900

    카운터모델 스키마 설정

--- node/model/postSchema.js
+++ node/model/postSchema.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema({
-		title: String,
-		content: String,
+const counterSchema = new mongoose.Schema(
+	{
+		name: String,
+		communityNum: Number,
 	},
-	{collection: 'Community'})
-;
+	{ collection: 'Counter' }
+);
 
-const Post = mongoose.model('Post', postSchema);
-module.exports = {Post};
+const Counter = mongoose.model('Counter', counterSchema);
+module.exports = { Counter };
