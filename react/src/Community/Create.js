cc62df6938e630bcb2a9027191943779ddb4c98b
jungmin <43zhuzhu@gmail.com>
2022-06-04 15:14:47 +0900

    react버전으로 인한 오류해결

--- react/src/Community/Create.js
+++ react/src/Community/Create.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { BrowserRouter as useNavigate } from 'react-router-dom';
 import Layout from '../Common/Layout';
 
 function Create() {
