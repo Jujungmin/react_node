223dbff80c1ee5fc2467d5008335dbd2f2568237
jungmin <43zhuzhu@gmail.com>
2022-06-04 15:10:53 +0900

    공통레이아웃적용

---
+++ react/src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './Common/Layout';
+
+function Main() {
+	return (
+		<Layout name={'Main'}>
+			<p>메인페이지입니다.</p>
+		</Layout>
+	)
+}
+
+export default Main;
